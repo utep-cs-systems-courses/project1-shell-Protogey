91a11b87f5478ef5be7882a48203533f8db41ca5
student <protogeyorocks@gmail.com>
2020-09-15 23:56:18 -0600

    late night coding

--- demos/shellTest.py
+++ demos/shellTest.py
@@ -1,3 +1,4 @@
+#copied from MSTeams
 def parse(cmdString):
     outFile = None
     inFile = None
@@ -18,24 +19,48 @@ def parse(cmdString):
 
 import os, sys, re
 
-sys.ps1 = "user/home/"
+sys.ps1 = "$"
 UInput = " "
 
 while(UInput.lower() != "exit"):
-    print(sys.ps1,"\n")
-    UInput = input("$")
-    if(UInput.lower() == "exit"):
+    print(sys.ps1)
+    UInput = input()
+    #checks if the user wants to exit
+    if(UInput.split(' ')[0].lower() == "exit"):
         print("Exiting\n")
         sys.exit()
+    #checks if the input string is more than 2 words (command and arguement or
+    #input command output)
     if(len(UInput.split()) >= 2):
-        first = UInput.split(' ')[0]
-        second = UInput.split(' ')[1]
-        if(second.lower() == "<" or second.lower() == ">"):
-            parse(second)
-        elif(second.lower() == "|"):
-            print("Pipe\n")
+        rc = os.fork()
+        #Fork failed
+        if rc < 0:
+            print("fork failed")
+            sys.exit(1)
+        elif rc == 0:
+            print("rc==0\n")
+            first = UInput.split(' ')[0]
+            second = UInput.split(' ')[1]
+            #if the second word (command) is < or > 
+            if(second.lower() == "<" or second.lower() == ">"):
+                parse(second)
+            #if the second word (command) is | (pipe)
+            elif(second.lower() == "|"):
+                pr, pw = os.pipe()
+                for f in (pr, pw):
+                    os.set_inheritable(f, True)
+                args = ["wc","p3-exec.py"]
+
+                os.close(1)
+                os.dup(pw)
+                for fd in (pr, pw):
+                    os.close(fd)
+                print("Pipe\n")
+            #else unknown command inputted
+            else:
+                print("unknown command", UInput, "\n")
         else:
-            print("unknown command", second, "\n")
+            print("WE IN THE ELSE\n")
     else:
         print("unknown command", UInput, "\n")
 
