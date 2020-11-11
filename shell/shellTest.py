1eddc6d12eadd2bd9365756638ffa2ec3f829c6b
student <protogeyorocks@gmail.com>
2020-11-11 15:52:54 -0700

    fixed?

--- shell/shellTest.py
+++ shell/shellTest.py
@@ -11,8 +11,8 @@ def parse(cmdString):
     if '>' in cmdString:
         [cmd, outFile] = cmdString.split('>',1)
         outFile = outFile.strip()
-    if '<' in cmd:
-        [cmd, inFile] = cmd.split('<', 1)
+    if '<' in cmdString:
+        [cmd, inFile] = cmdString.split('<', 1)
         inFile = inFile.strip()
     elif outFile != None and '<' in outFile:
         [outFile, inFile] = outFile.split('<', 1)
@@ -47,7 +47,8 @@ def pipe(cmdString):
         os.dup2(pr, 0)#pipe output to input (fd0)
         for fd in (pr, pw):
             os.close(fd)#disconnect from pipe
-        exe(second)#replace memory
+        exe(second)
+        os.write(1, "Piped..!".encode())
 
 #exe commands, exec demo from lab
 def exe(cmd):
@@ -57,54 +58,57 @@ def exe(cmd):
         sys.exit(1)
     elif cFork == 0:
         cmd = cmd.split()
-        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-            program = "%s/%s" % (dir, cmd[0])
-            try:
-                os.execve(program, cmd, os.environ) # try to exec program
-            except FileNotFoundError:             # ...expected
-                pass                              # ...fail quietly
-            os.write(2, ("Child:    Could not exec %s\n" % cmd[0]).encode())
-            sys.exit(1)
+        command = "/bin/"+cmd[0]
+        try:
+            os.execve(command, cmd, os.environ)
+        except FileNotFoundError:
+            os.write(1, ("%s: command not found\n" % cmd[0]).encode())
+            pass
     else:
         cFork = os.wait()
 
 def exeOut(cmd):
+    cmd, outFile, inFile = parse(cmd)
+    os.write(1, (" out: %s" % outFile).encode())
+    os.write(1, (" in: %s \n" % inFile).encode())
     cFork = os.fork()
     if cFork < 0:
         os.write(1, "Fork failed -exe".encode())
         sys.exit(1)
     elif cFork == 0:
-        cmd = cmd.split()
+        #cmd = cmd.split()
+        command = "/bin/"+cmd[0]
         os.close(1) #redirect stdout
+        os.open(inFile, os.O_CREAT | os.O_WRONLY);
         os.set_inheritable(1, True)
-        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-            program = "%s/%s" % (dir, cmd[0])
-            try:
-                os.execve(program, cmd, os.environ) # try to exec program
-            except FileNotFoundError:             # ...expected
-                pass                              # ...fail quietly
-            os.write(2, ("Child:    Could not exec %s\n" % cmd[0]).encode())
-            sys.exit(1)
+        try:
+            os.execve(command, cmd, os.environ)
+        except FileNotFoundError:
+            os.write(1, ("%s: command not found\n" % cmd[0]).encode())
+            pass
     else:
         cFork = os.wait()
 
 def exeIn(cmd):
+    cmd, outFile, inFile = parse(cmd)
+    os.write(1, (cmd[0]).encode())
+    os.write(1, (" out: %s" %outFile).encode())
+    os.write(1, (" in: %s \n" % inFile).encode())
     cFork = os.fork()
     if cFork < 0:
         os.write(1, "Fork failed -exe".encode())
         sys.exit(1)
     elif cFork == 0:
-        cmd = cmd.split()
-        os.close(0) #redirect stdin
-        os.set_inheritable(0, True)
-        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-            program = "%s/%s" % (dir, cmd[0])
-            try:
-                os.execve(program, cmd, os.environ) # try to exec program
-            except FileNotFoundError:             # ...expected
-                pass                              # ...fail quietly
-            os.write(2, ("Child:    Could not exec %s\n" % cmd[0]).encode())
-            sys.exit(1)
+        #cmd = cmd.split()
+        command = "/bin/"+cmd[0]
+        os.close(1) #redirect stdin
+        os.open(outFile, os.O_CREAT | os.O_WRONLY);
+        os.set_inheritable(1, True)
+        try:
+            os.execve(command, cmd, os.environ)
+        except FileNotFoundError:
+            os.write(1, ("%s: command no found\n" % cmd[0]).encode())
+            pass
     else:
         cFork = os.wait()
         
@@ -124,24 +128,6 @@ while(UInput != "exit"):
     if("exit" in UInput):
         os.write(1, ("exiting\n").encode())
         sys.exit()
-    #checks if the user wants to ls
-    if("ls" in UInput):
-        flag = True
-        list = os.listdir(directory)
-        for file in list:
-            os.write(1, file.encode())
-            os.write(1, "\n".encode())
-    #checks if the user wants to see the current dir
-    if("dir" in UInput):
-        flag = True
-        directory = os.getcwd()
-        os.write(1, directory.encode())
-        os.write(1, "\n".encode())
-    if("pwd" in UInput):
-        flag = True
-        directory = os.getcwd()
-        os.write(1, directory.encode())
-        os.write(1, "\n".encode())
     #if pipe is used anywhere in the input
     if('|' in UInput):
         flag = True
@@ -149,28 +135,23 @@ while(UInput != "exit"):
     #if the second word (command) is < or > 
     if('<' in UInput or '>' in UInput):
         flag = True
-        cmd, outFile, inFile = parse(UInput)
         if('<' in UInput):
-            os.write(1, "redirecting output".encode())
-            exeOut(UInput.split('<')[1])
+            os.write(1, "redirecting output\n".encode())
+            exeOut(UInput)
         elif('>' in UInput):
-            os.write(1, "redirecting input".encode())
-            exeIn(UInput.split('>')[0])
+            os.write(1, "redirecting input\n".encode())
+            exeIn(UInput)
         else:
             os.write(1, "weird bug!".encode())    
-    #echo command
-    if("echo " in UInput):
-        flag = True
-        os.write(1, UInput.split()[1].encode())
-        os.write(1, "\n".encode())
     #else unknown command inputted
     if("cd " in UInput):
         flag = True
         os.chdir(UInput.split()[1].encode())
         os.write(1, UInput.split()[1].encode())
+    if("dir" in UInput):
+       flag = True
+       os.write(1, (os.getcwd()+"\n").encode())
     if(flag):
         pass
     else:
-        os.write(1, "unknown command: ".encode())
-        os.write(1, UInput.split(' ')[0].encode())
-        os.write(1, "\n".encode())
+        exe(UInput)
